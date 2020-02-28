6e93ceaefdd55bd3e7ad1b58033d30319aacf165
김경록 <goodboy3421@gmail.com>
2020-02-28 21:23:46 +0900

    feat: Lotto 상수, 출력기능 추가
    toString 메서드, 상수 추가

--- src/main/java/domain/Lotto.java
+++ src/main/java/domain/Lotto.java
@@ -2,15 +2,32 @@ package domain;
 
 import java.util.List;
 
+import static java.util.stream.Collectors.joining;
+
 /**
  * 로또 한장을 의미하는 객체
  */
 public class Lotto {
+
+    public static final int MIN_LOTTO_NUMBER = 1;
+    public static final int MAX_LOTTO_NUMBER = 45;
+    public static final int LOTTO_LENGTH = 6;
+    public static final int LOTTO_UNIT_PRICE = 1000;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         this.numbers = numbers;
     }
 
-    // 추가 기능 구현
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + numbers.stream()
+                .sorted()
+                .map(String::valueOf)
+                .collect(joining(", ")) + "]";
+    }
 }
