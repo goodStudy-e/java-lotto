ac034563a6a91389a1d73015e706c1d4aa430176
김경록 <goodboy3421@gmail.com>
2020-02-28 21:53:25 +0900

    refactor: LottoGenerator 리팩토링
    하드코딩, 함수 길이(10미만) 요구사항 반영하여 리팩토링

--- src/main/java/utility/LottoGenerator.java
+++ src/main/java/utility/LottoGenerator.java
@@ -7,21 +7,24 @@ import java.util.List;
 import java.util.Random;
 
 public class LottoGenerator {
-    static private int LOTTO_LIMIT_LENGTH = 6;
+    static private final int LOTTO_LIMIT_LENGTH = 6;
 
     static Lotto generateLotto() {
         List<Integer> lottoNumbers = new ArrayList<>();
 
-        Random random = new Random();
-        for (int i = 0; i < LOTTO_LIMIT_LENGTH; i++) { //while로 바꾸면 depth 1 가능할 듯
-            int randomNumber = Lotto.MIN_LOTTO_NUMBER + random.nextInt(Lotto.MAX_LOTTO_NUMBER); //1 ~ 45
-
-            if (lottoNumbers.contains(randomNumber)) { //todo: indent depth 1까지만 허용! (리팩토링)
-                --i; // 이게 리팩토링의 문제점
-                continue;
-            }
-            lottoNumbers.add(randomNumber);
+        for (int i = 0; i < LOTTO_LIMIT_LENGTH; i++) {
+            isDuplicated(lottoNumbers);
         }
         return new Lotto(lottoNumbers);
+    }
+
+    private static void isDuplicated(List<Integer> lottoNumbers) {
+        Random random = new Random();
+        int randomNumber = Lotto.MIN_LOTTO_NUMBER + random.nextInt(Lotto.MAX_LOTTO_NUMBER); //1 ~ 45
+        if (lottoNumbers.contains(randomNumber)) {
+            isDuplicated(lottoNumbers);
+            return;
+        }
+        lottoNumbers.add(randomNumber);
     }
 }
