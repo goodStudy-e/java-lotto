67ada8754e20fb78e2c8249032fa24770fa57f02
김경록 <goodboy3421@gmail.com>
2020-02-28 23:15:48 +0900

    refactor: LottoResult 리팩토링
    함수 길이(10미만) 요구사항 반영하여 리팩토링

--- src/main/java/domain/LottoResult.java
+++ src/main/java/domain/LottoResult.java
@@ -1,56 +1,73 @@
 package domain;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class LottoResult {
+    private static final int PERCENTAGE = 100;
 
     public static void printWinningStatistics(List<Lotto> userLottos, WinningLotto winningLotto) {
-        List<Rank> userRanks = getUserRanks(userLottos, winningLotto);
+        List<Rank> userRanks = getUserRemovedMISSRanks(userLottos, winningLotto);
+        Map<Rank, Integer> rankMap = getRankMap(userRanks, new HashMap<>());
+        long purchaseMoney = Lotto.LOTTO_UNIT_PRICE * userLottos.size();
+
         System.out.println("당첨통계");
         System.out.println("---------");
-        Map<Rank, Integer> rankMap = new HashMap<>();
-
-        for (Rank rank : userRanks) {
-            if (!rankMap.containsKey(rank)) {
-                rankMap.put(rank, 1);
-                continue;
-            }
-            rankMap.put(rank, rankMap.get(rank) + 1);
-        }
-
-        long sum = 0;
-        for (Rank rank : Rank.values()) { //todo: 리팩토링
-            if (rank.equals(Rank.MISS)) {
-                continue;
-            }
-
-            long rankWinningMoney = rank.getWinningMoney();
-            int rankNumber = rankMap.getOrDefault(rank, 0);
-
-            System.out.print(rank.getCountOfMatch() + "개일치");
-            if (rank.equals(Rank.SECOND)) {
-                System.out.print(", 보너스 볼 일치");
-            }
-            System.out.print("(" + rankWinningMoney + "원)- ");
-            System.out.println(rankMap.getOrDefault(rank, 0) + "개");
-            System.out.println("추가금액: " + rankWinningMoney * rankNumber);
-            sum += rankWinningMoney * rankNumber;
-        }
-
-        long purchaseMoney = Lotto.LOTTO_UNIT_PRICE * userLottos.size();
-        double earningsRate = (double) sum / purchaseMoney * 100;
+        double earningsRate = (double) getTotalWinningMoney(rankMap) / purchaseMoney * PERCENTAGE;
         System.out.println("총 수익률은 " + earningsRate + "% 입니다.");
     }
 
-    private static List<Rank> getUserRanks(List<Lotto> userLottos, WinningLotto winningLotto) {
-        List<Rank> userRanks = new ArrayList<>();
-
-        for (Lotto userLotto : userLottos) { //todo: stream 으로 변환 가능할까?
-            userRanks.add(winningLotto.match(userLotto));
+    private static long getTotalWinningMoney(Map<Rank, Integer> rankMap) {
+        long sum = 0;
+        for (Rank rank : Rank.values()) {
+            printRank(rankMap, rank);
+            sum += getRankSum(rank, rankMap);
         }
-        return userRanks;
+        return sum;
+    }
+
+    private static long getRankSum(Rank rank, Map<Rank, Integer> rankMap) {
+        long rankWinningMoney = rank.getWinningMoney();
+        int rankNumber = rankMap.getOrDefault(rank, 0);
+        return rankWinningMoney * rankNumber;
+    }
+
+    private static void printRank(Map<Rank, Integer> rankMap, Rank rank) {
+        long rankWinningMoney = rank.getWinningMoney();
+        int rankNumber = rankMap.getOrDefault(rank, 0);
+
+        System.out.print(rank.getCountOfMatch() + "개일치");
+        isEqualToSECOND(rank);
+        System.out.print("(" + rankWinningMoney + "원)- ");
+        System.out.println(rankNumber + "개");
+        System.out.println("추가금액: " + getRankSum(rank, rankMap));
+    }
+
+
+    private static void isEqualToSECOND(Rank rank) {
+        if (rank.equals(Rank.SECOND)) {
+            System.out.print(", 보너스 볼 일치");
+        }
+    }
+
+    private static Map<Rank, Integer> getRankMap(List<Rank> userRanks, Map<Rank, Integer> rankMap) {
+        userRanks.stream()
+                .forEach(rank -> {
+                    if (rankMap.containsKey(rank)) {
+                        rankMap.put(rank, rankMap.get(rank) + 1);
+                    } else {
+                        rankMap.put(rank, 1);
+                    }
+                });
+        return rankMap;
+    }
+
+    private static List<Rank> getUserRemovedMISSRanks(List<Lotto> userLottos, WinningLotto winningLotto) {
+        return userLottos.stream()
+                .map(userLotto -> winningLotto.match(userLotto))
+                .filter(rank -> !rank.equals(Rank.MISS))
+                .collect(Collectors.toList());
     }
 }
