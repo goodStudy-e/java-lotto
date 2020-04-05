4a32f6279a7bceaab30697a56846ca6c1d38d55a
김경록 <goodboy3421@gmail.com>
2020-04-06 00:42:08 +0900

    style: 로또게임 main부분 띄워쓰기하여 코드 포맷팅

--- src/main/java/LottoGame.java
+++ src/main/java/LottoGame.java
@@ -10,9 +10,11 @@ public class LottoGame {
         PurchaseMoney purchaseMoney = InputManager.inputPurchaseMoney(); //구입금액
         List<Lotto> userLottos = InputManager.getUserLottos(purchaseMoney.getAmount()); // 유저의 로또
         WinningLotto winningLotto = InputManager.inputWinningLotto(); // 당첨번호, 보너스볼 입력
+
         List<Rank> userWinningLottos = LottoResult.getUserWinningLottos(userLottos, winningLotto); // 유저의 당첨된 로또들
         Map<Rank, Integer> lottosByRank = LottoResult.getLottosByRank(userWinningLottos);
-        double earningRate = LottoResult.getEarningRate(lottosByRank, purchaseMoney);
-        OutputManager.printResult(lottosByRank, earningRate);
+        double earningrate = LottoResult.getEarningRate(lottosByRank, purchaseMoney);
+
+        OutputManager.printResult(lottosByRank, earningrate);
     }
 }
