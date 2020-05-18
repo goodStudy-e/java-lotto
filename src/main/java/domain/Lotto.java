c81268f64f392bd33b5f2ed4cea7d326292799bb
김경록 <goodboy3421@gmail.com>
2020-05-18 18:59:54 +0900

    refactor: Lotto Model의 필드값 주소를 노출하지 않게 변경

--- src/main/java/domain/Lotto.java
+++ src/main/java/domain/Lotto.java
@@ -1,33 +1,21 @@
 package domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import static java.util.stream.Collectors.joining;
-
-/**
- * 로또 한장을 의미하는 객체
- */
 public class Lotto {
-
     public static final int MIN_LOTTO_NUMBER = 1;
     public static final int MAX_LOTTO_NUMBER = 45;
     public static final int LOTTO_LENGTH = 6;
     public static final int LOTTO_UNIT_PRICE = 1000;
+
     private final List<Integer> numbers;
 
-    public Lotto(List<Integer> numbers) {
-        this.numbers = numbers;
+    public Lotto(final List<Integer> numbers) {
+        this.numbers = new ArrayList<>(numbers);
     }
 
     public List<Integer> getNumbers() {
-        return numbers;
-    }
-
-    @Override
-    public String toString() {
-        return "[" + numbers.stream()
-                .sorted()
-                .map(String::valueOf)
-                .collect(joining(", ")) + "]";
+        return new ArrayList<>(numbers);
     }
 }
