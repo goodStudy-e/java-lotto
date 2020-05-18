bb91a7ce25171c12bb16a98cfa276dda07d7bd3a
김경록 <goodboy3421@gmail.com>
2020-05-18 17:36:38 +0900

    test: 표준 예외로 변경하여 테스트 코드도 리팩토링

--- src/test/java/domain/PurchaseMoneyTest.java
+++ src/test/java/domain/PurchaseMoneyTest.java
@@ -2,7 +2,6 @@ package domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import utility.InputException;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -11,14 +10,14 @@ class PurchaseMoneyTest {
     @DisplayName("구입 금액이 0보다 작은 경우에는 예외를 던진다.")
     void throwException_ifAmountIsLowerThan0() {
         assertThatThrownBy(() -> new PurchaseMoney(-1))
-                .isExactlyInstanceOf(InputException.class);
+                .isExactlyInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     @DisplayName("구입 금액이 1000의 배수가 아니면 예외를 던진다.")
     void throwException_ifAmountIsNot1000Multiple() {
         assertThatThrownBy(() -> new PurchaseMoney(1001))
-                .isExactlyInstanceOf(InputException.class);
+                .isExactlyInstanceOf(IllegalArgumentException.class);
     }
 
 }
