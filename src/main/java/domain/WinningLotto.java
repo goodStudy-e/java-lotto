a7e24b9f1bbcd623b458a1e287695cb01e2630d0
김경록 <goodboy3421@gmail.com>
2020-05-24 03:18:50 +0900

    refactor: bonusNo를 BonusBall Wrapper Class로 리팩토링

--- src/main/java/domain/WinningLotto.java
+++ src/main/java/domain/WinningLotto.java
@@ -1,23 +1,27 @@
 package domain;
 
-/**
- * 당첨 번호를 담당하는 객체
- */
 public class WinningLotto {
     private final Lotto lotto;
-    private final int bonusNo;
+    private final BonusBall bonusBall;
 
-    public WinningLotto(Lotto lotto, int bonusNo) {
-        this.lotto = lotto;
-        this.bonusNo = bonusNo;
+    public WinningLotto(Lotto lotto, BonusBall bonusBall) {
+        this.lotto = new Lotto(lotto.getNumbers());
+        this.bonusBall = new BonusBall(bonusBall.getNumber(), lotto);
     }
 
     public Rank match(Lotto userLotto) {
         int countOfMatch = (int) lotto.getNumbers().stream()
                 .filter(number -> userLotto.getNumbers().contains(number))
                 .count();
-        boolean matchBonus = userLotto.getNumbers().contains(bonusNo);
-
+        boolean matchBonus = userLotto.getNumbers().contains(bonusBall.getNumber());
         return Rank.valueOf(countOfMatch, matchBonus);
+    }
+
+    public Lotto getLotto() {
+        return new Lotto(lotto.getNumbers());
+    }
+
+    public BonusBall getBonusBall() {
+        return bonusBall;
     }
 }
