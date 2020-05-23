67a7c50f5986e8b30a74c0b0d78899996c3cdb45
김경록 <goodboy3421@gmail.com>
2020-05-23 23:43:06 +0900

    fix: THIRD를 SECOND로 매칭하는 버그 수정

--- src/main/java/domain/Rank.java
+++ src/main/java/domain/Rank.java
@@ -1,8 +1,5 @@
 package domain;
 
-/**
- * 로또 등수를 의미하는 enum
- */
 public enum Rank {
     FIRST(6, 2_000_000_000), // 1등
     SECOND(5, 30_000_000), // 2등
@@ -11,12 +8,13 @@ public enum Rank {
     FIFTH(3, 5_000), // 5등
     MISS(0, 0);
 
+    private static final String COUNT_OF_MATCH_INVALID_MESSAGE = "는 유효하지 않은 값입니다.";
     private static final int WINNING_MIN_COUNT = 3;
 
     private int countOfMatch;
     private int winningMoney;
 
-    private Rank(int countOfMatch, int winningMoney) {
+    Rank(int countOfMatch, int winningMoney) {
         this.countOfMatch = countOfMatch;
         this.winningMoney = winningMoney;
     }
@@ -34,17 +32,20 @@ public enum Rank {
             return MISS;
         }
 
-        if (SECOND.matchCount(countOfMatch) && matchBonus) {
+        if (SECOND.matchCount(countOfMatch) && matchBonus) { //todo: 이부분이 적용이 안됌 ...ㅠㅠ 
             return SECOND;
         }
 
+        if (THIRD.matchCount(countOfMatch) && !matchBonus) {
+            return THIRD;
+        }
+
         for (Rank rank : values()) {
-            if (rank.matchCount(countOfMatch)) {
+            if (rank.matchCount(countOfMatch)) { //SECOND에 대한 예외처리 안돼있음
                 return rank;
             }
         }
-
-        throw new IllegalArgumentException(countOfMatch + "는 유효하지 않은 값입니다.");
+        throw new IllegalArgumentException(countOfMatch + COUNT_OF_MATCH_INVALID_MESSAGE);
     }
 
     private boolean matchCount(int countOfMatch) {
