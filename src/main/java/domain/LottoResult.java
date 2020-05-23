842c0fcfd51219f70f57525ebf340f0eec2865fc
김경록 <goodboy3421@gmail.com>
2020-05-24 03:01:56 +0900

    refactor: LottoGame의 결과를 담은 LottoResult 리팩토링
    접근제어자, 메서드명, 필드 변경 및 추가

--- src/main/java/domain/LottoResult.java
+++ src/main/java/domain/LottoResult.java
@@ -1,37 +1,44 @@
 package domain;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import static java.util.stream.Collectors.*;
-
 public class LottoResult {
     private static final int PERCENTAGE = 100;
 
-    public static List<Rank> getUserWinningLottos(List<Lotto> userLottos, WinningLotto winningLotto) {
-        return userLottos.stream()
-                .map(winningLotto::match)
-                .filter(rank -> !rank.equals(Rank.MISS))
-                .collect(Collectors.toList());
+    private List<Lotto> userLottos;
+    private WinningLotto winningLotto;
+
+    public LottoResult(List<Lotto> userLottos, WinningLotto winningLotto) {
+        this.userLottos = new ArrayList<>(userLottos);
+        this.winningLotto = new WinningLotto(winningLotto.getLotto(), winningLotto.getBonusBall());
     }
 
-    public static double getEarningRate(Map<Rank, Integer> numberOfWinningLottosByRank, PurchaseMoney purchaseMoney) {
-        return (double) getTotalWinningMoney(numberOfWinningLottosByRank) / purchaseMoney.getAmount() * PERCENTAGE;
+    public double calculateEarningRate(Map<Rank, Integer> lottoByRank, PurchaseMoney purchaseMoney) {
+        return (double) getTotalWinningMoney(lottoByRank) / purchaseMoney.getAmount() * PERCENTAGE;
     }
 
-    private static long getTotalWinningMoney(Map<Rank, Integer> rankMap) {
+    private long getTotalWinningMoney(Map<Rank, Integer> rankMap) {
         return Arrays.stream(Rank.values())
                 .mapToLong(rank -> rank.getWinningMoney() * rankMap.getOrDefault(rank, 0))
                 .sum();
     }
 
-    public static Map<Rank, Integer> getLottosByRank(List<Rank> userRanks) {
+    public Map<Rank, Integer> getLottosByRank() {
+        List<Rank> userRanks = getUserWinningLottos();
         return userRanks.stream()
-                .collect(groupingBy(rank -> rank))
+                .collect(Collectors.groupingBy(rank -> rank))
                 .entrySet().stream()
                 .collect(Collectors.toMap(Map.Entry::getKey, entry -> entry.getValue().size()));
     }
 
+    private List<Rank> getUserWinningLottos() {
+        return userLottos.stream()
+                .map(winningLotto::match)
+                .filter(rank -> !rank.equals(Rank.MISS))
+                .collect(Collectors.toList());
+    }
 }
