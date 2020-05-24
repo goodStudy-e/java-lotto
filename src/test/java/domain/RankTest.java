d6d4c6ee21a617f5b38b23064b4732c456009f97
김경록 <goodboy3421@gmail.com>
2020-05-24 18:33:06 +0900

    test: Rank의 기능을 테스트하는 테스트 코드 추가

---
+++ src/test/java/domain/RankTest.java
@@ -0,0 +1,36 @@
+package domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RankTest {
+
+    @DisplayName("당첨된 복권 등수를 조회하는 테스트한다")
+    @Test
+    void test_valueOfFunction() {
+        //given
+        int countOfMatchSix = 6;
+        int countOfMatchFive = 5;
+        int countOfMatchFour = 4;
+        int countOfMatchThree = 3;
+        int countOfMatchTwo = 2;
+
+        //then
+        Rank answer1 = Rank.valueOf(countOfMatchSix, false);
+        Rank answer2 = Rank.valueOf(countOfMatchFive, true);
+        Rank answer3 = Rank.valueOf(countOfMatchFive, false);
+        Rank answer4 = Rank.valueOf(countOfMatchFour, false);
+        Rank answer5 = Rank.valueOf(countOfMatchThree, false);
+        Rank answer6 = Rank.valueOf(countOfMatchTwo, false);
+
+        //then
+        assertThat(answer1).isEqualTo(Rank.FIRST);
+        assertThat(answer2).isEqualTo(Rank.SECOND);
+        assertThat(answer3).isEqualTo(Rank.THIRD);
+        assertThat(answer4).isEqualTo(Rank.FOURTH);
+        assertThat(answer5).isEqualTo(Rank.FIFTH);
+        assertThat(answer6).isEqualTo(Rank.MISS);
+    }
+}
