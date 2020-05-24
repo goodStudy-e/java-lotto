e609f9d386a9b9705c8aa6c475d522c9bd2d7111
김경록 <goodboy3421@gmail.com>
2020-05-24 19:13:08 +0900

    refactor: 단일책임원칙을 위반하여 기능 분리
    이득률을 계산하는 기능은 다른 객체에게 역할을 부여한다.

--- src/main/java/domain/LottoResult.java
+++ src/main/java/domain/LottoResult.java
@@ -1,30 +1,17 @@
 package domain;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 public class LottoResult {
-    private static final int PERCENTAGE = 100;
-
-    private List<Lotto> userLottos;
-    private WinningLotto winningLotto;
+    private final List<Lotto> userLottos;
+    private final WinningLotto winningLotto;
 
     public LottoResult(List<Lotto> userLottos, WinningLotto winningLotto) {
         this.userLottos = new ArrayList<>(userLottos);
         this.winningLotto = new WinningLotto(winningLotto.getLotto(), winningLotto.getBonusBall());
-    }
-
-    public double calculateEarningRate(Map<Rank, Integer> lottoByRank, PurchaseMoney purchaseMoney) {
-        return (double) getTotalWinningMoney(lottoByRank) / purchaseMoney.getAmount() * PERCENTAGE;
-    }
-
-    private long getTotalWinningMoney(Map<Rank, Integer> rankMap) {
-        return Arrays.stream(Rank.values())
-                .mapToLong(rank -> rank.getWinningMoney() * rankMap.getOrDefault(rank, 0))
-                .sum();
     }
 
     public Map<Rank, Integer> getLottosByRank() {
