f4b936e1ecd333dada99dd9a847af03877eb0901
김경록 <goodboy3421@gmail.com>
2020-05-24 19:24:45 +0900

    feat: 이득률을 계산하는 기능 추가

---
+++ src/main/java/domain/Calculator.java
@@ -0,0 +1,18 @@
+package domain;
+
+import java.util.Arrays;
+import java.util.Map;
+
+public class Calculator {
+    private static final int PERCENTAGE = 100;
+
+    public static double calculateEarningRate(Map<Rank, Integer> lottoByRank, PurchaseMoney purchaseMoney) {
+        return (double) getTotalWinningMoney(lottoByRank) / purchaseMoney.getAmount() * PERCENTAGE;
+    }
+
+    private static long getTotalWinningMoney(Map<Rank, Integer> rankMap) {
+        return Arrays.stream(Rank.values())
+                .mapToLong(rank -> rank.getWinningMoney() * rankMap.getOrDefault(rank, 0))
+                .sum();
+    }
+}
