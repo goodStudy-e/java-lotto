720c152b20a810a353033ca7d74811569fb85ad4
김경록 <goodboy3421@gmail.com>
2020-05-24 18:22:07 +0900

    test: WinningLotto의 테스트 코드 추가

---
+++ src/test/java/domain/WinningLottoTest.java
@@ -0,0 +1,44 @@
+package domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class WinningLottoTest {
+
+    @DisplayName("당첨복권을 생성한다")
+    @Test
+    void test_generateWinningLotto() {
+        //given
+        Lotto lotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
+        int bonusBallNumber = 7;
+        BonusBall bonusBall = new BonusBall(bonusBallNumber, lotto);
+
+        //when
+        WinningLotto winningLotto = new WinningLotto(lotto, bonusBall);
+
+        //then
+        assertThat(winningLotto.getBonusBall().getNumber()).isEqualTo(bonusBallNumber);
+        assertThat(winningLotto.getLotto().getNumbers()).isEqualTo(lotto.getNumbers());
+    }
+
+    @DisplayName("복권의 당첨여부를 확인하는 기능을 테스트한다")
+    @Test
+    void test_matchFunction() {
+        //given
+        Lotto lotto = new Lotto(Arrays.asList(1,2,3,4,5,6));
+        Lotto userLotto = new Lotto(Arrays.asList(1,2,3,4,5,10));
+        int bonusNumber = 7;
+        BonusBall bonusBall = new BonusBall(bonusNumber, lotto);
+
+        //when
+        WinningLotto winningLotto = new WinningLotto(lotto, bonusBall);
+        Rank answer = winningLotto.match(userLotto);
+
+        //then
+        assertThat(answer).isEqualTo(Rank.THIRD);
+    }
+}
