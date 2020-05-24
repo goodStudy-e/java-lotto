9eb6fd272a54d4e3879080f23d6105fdf51f599d
김경록 <goodboy3421@gmail.com>
2020-05-24 19:14:26 +0900

    test: LottoReuslt를 리팩토링에따 테스트른코드 제거

--- src/test/java/domain/LottoResultTest.java
+++ src/test/java/domain/LottoResultTest.java
@@ -3,69 +3,38 @@ package domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class LottoResultTest {
 
-    @DisplayName("당첨된로또를_구한다")
+    @DisplayName("당첨된 로또를 구하는 기능을 테스트한다")
     @Test
     void test_getUserWinningLottos() {
         //given
-        List<Lotto> userLottos = List.of(
-                new Lotto(List.of(1, 2, 3, 4, 5, 6)), //FIRST(6개일치) - 1개
-                new Lotto(List.of(1, 2, 3, 4, 5, 32)), //SECOND(5개일치,보너스일치) - 1개
-                new Lotto(List.of(1, 2, 3, 4, 32, 43)), //FOURTH(4개일치) - 1개
-                new Lotto(List.of(1, 2, 10, 15, 22, 34)), //2개일치 MISS
-                new Lotto(List.of(12, 11, 3, 20, 27, 32)) //2개일치 MISS
+        List<Lotto> userLottos = Arrays.asList(
+                new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6)), //FIRST(6개일치) - 1개
+                new Lotto(Arrays.asList(1, 2, 3, 4, 5, 32)), //SECOND(5개일치,보너스일치) - 1개
+                new Lotto(Arrays.asList(1, 2, 3, 4, 5, 33)), //SECOND(5개일치,보너스불일치) - 1개
+                new Lotto(Arrays.asList(1, 2, 3, 4, 32, 43)), //FOURTH(4개일치) - 1개
+                new Lotto(Arrays.asList(1, 2, 10, 15, 22, 34)), //2개일치 MISS
+                new Lotto(Arrays.asList(12, 11, 3, 20, 27, 32)) //2개일치 MISS
         );
-
-        WinningLotto winningLotto = new WinningLotto(new Lotto(List.of(1, 2, 3, 4, 5, 6)), 7);
+        Lotto lotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
+        WinningLotto winningLotto = new WinningLotto(lotto, new BonusBall(32, lotto));
 
         //when
-        List<Rank> userWinningLottos = LottoResult.getUserWinningLottos(userLottos, winningLotto);
+        LottoResult lottoResult = new LottoResult(userLottos, winningLotto);
+        Map<Rank, Integer> userWinningLottos = lottoResult.getLottosByRank();
 
         //then
-        assertThat(3).isEqualTo(userWinningLottos.size());
-    }
-
-    @DisplayName("사용자의_당첨로또를_등수별로_나눈다")
-    @Test
-    void test_getLottosByRank() {
-        //given
-        List<Rank> userWinningLottos = List.of(
-                Rank.FIRST,
-                Rank.SECOND,
-                Rank.FOURTH
-        );
-
-        //when
-        Map<Rank, Integer> userLottosByRank = LottoResult.getLottosByRank(userWinningLottos);
-
-        //then
-        assertThat(1).isEqualTo(userLottosByRank.get(Rank.FIRST));
-        assertThat(1).isEqualTo(userLottosByRank.get(Rank.SECOND));
-        assertThat(1).isEqualTo(userLottosByRank.get(Rank.FOURTH));
-        assertThat(3).isEqualTo(userLottosByRank.size());
-    }
-
-    @DisplayName("이득률을_구한다")
-    @Test
-    void test_getEarningRate() {
-        //given
-        PurchaseMoney purchaseMoney = new PurchaseMoney(3000);
-        Map<Rank, Integer> userWinningLottos = Map.of(
-                Rank.THIRD, 1, // 1,500,000
-                Rank.FOURTH, 1 // 50,000
-        );
-
-        //when
-        double earningRate = LottoResult.getEarningRate(userWinningLottos, purchaseMoney);
-
-        //then
-        double expectedEarningRate = (double) (1500000 + 50000) / purchaseMoney.getAmount() * 100;
-        assertThat(expectedEarningRate).isEqualTo(earningRate);
+        assertThat(userWinningLottos).hasSize(4);
+        assertThat(userWinningLottos.get(Rank.FIRST)).isEqualTo(1);
+        assertThat(userWinningLottos.get(Rank.SECOND)).isEqualTo(1);
+        assertThat(userWinningLottos.get(Rank.THIRD)).isEqualTo(1);
+        assertThat(userWinningLottos.get(Rank.FOURTH)).isEqualTo(1);
     }
 }
