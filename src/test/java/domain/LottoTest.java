bccb33f5a17a99aa70f68c5109ffe15aa7d7f22c
김경록 <goodboy3421@gmail.com>
2020-05-24 16:43:21 +0900

    test: Lotto의 기능을 테스트하는 테스트 코드 추가
    Lotto 객체 생성, 예외 처리 기능 테스트

--- src/test/java/domain/LottoTest.java
+++ src/test/java/domain/LottoTest.java
@@ -3,18 +3,67 @@ package domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
-
-    @DisplayName("Lotto를_출력한다")
+    @DisplayName("로또를 생성한다")
     @Test
-    void LottoToString() {
-        List<Integer> lottoNumber = List.of(1, 2, 3, 4, 5, 6);
+    void test_createLotto() {
+        //given
+        List<Integer> lottoNumber = Arrays.asList(1, 2, 3, 4, 5, 6);
+
+        //when
         Lotto lotto = new Lotto(lottoNumber);
-        assertThat(1 + 2).isEqualTo(3);
-        assertThat("[1, 2, 3, 4, 5, 6]").isEqualTo(lotto.toString());
+
+        //then
+        assertThat(lotto.getNumbers()).isEqualTo(lottoNumber);
+    }
+
+    @DisplayName("로또번호의 길이가 다르면 예외를 발생시킨다")
+    @Test
+    void test_LottoNumberLengthException() {
+        //given
+        List<Integer> lottoNumbers = Arrays.asList(1, 2, 3, 4, 5);
+
+        //when //then
+        assertThatThrownBy(() -> new Lotto(lottoNumbers))
+                .isExactlyInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또번호가 중복되면 예외를 발생시킨다")
+    @Test
+    void test_LottoNumberDuplication() {
+        //given
+        List<Integer> lottoNumbers = Arrays.asList(1, 2, 3, 4, 5, 5);
+
+        //when //then
+        assertThatThrownBy(() -> new Lotto(lottoNumbers))
+                .isExactlyInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또번호 중에 로또숫자 범위보다 큰 값이 있으면 예외를 발생시킨다")
+    @Test
+    void test_LottoNumberOverRange() {
+        //given
+        List<Integer> lottoNumbers = Arrays.asList(1, 2, 3, 4, 5, 46);
+
+        //when //then
+        assertThatThrownBy(() -> new Lotto(lottoNumbers))
+                .isExactlyInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또번호 중에 로또숫자 범위보다 작은 값이 있으면 예외를 발생시킨다")
+    @Test
+    void test_LottoNumberUnderRange() {
+        //given
+        List<Integer> lottoNumbers = Arrays.asList(0, 1, 2, 3, 4, 5);
+
+        //when //then
+        assertThatThrownBy(() -> new Lotto(lottoNumbers))
+                .isExactlyInstanceOf(IllegalArgumentException.class);
     }
 }
