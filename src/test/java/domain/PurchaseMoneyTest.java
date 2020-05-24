f7d170b6d29e806a9a979caa6870205dc681a30e
김경록 <goodboy3421@gmail.com>
2020-05-24 17:56:06 +0900

    test: PurchaseMoneyTest 리팩토링

--- src/test/java/domain/PurchaseMoneyTest.java
+++ src/test/java/domain/PurchaseMoneyTest.java
@@ -8,16 +8,15 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class PurchaseMoneyTest {
     @Test
     @DisplayName("구입 금액이 0보다 작은 경우에는 예외를 던진다.")
-    void throwException_ifAmountIsLowerThan0() {
+    void throwException_ifAmountIsLowerThanZero() {
         assertThatThrownBy(() -> new PurchaseMoney(-1))
                 .isExactlyInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     @DisplayName("구입 금액이 1000의 배수가 아니면 예외를 던진다.")
-    void throwException_ifAmountIsNot1000Multiple() {
+    void throwException_ifAmountIsNotMultipleOfPrice() {
         assertThatThrownBy(() -> new PurchaseMoney(1001))
                 .isExactlyInstanceOf(IllegalArgumentException.class);
     }
-
 }
